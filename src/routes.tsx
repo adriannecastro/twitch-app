1fdc0ecbdc5b84673d4a4186b1132156b2c55910
Adrianne Castro <adrianneccastro@gmail.com>
2020-09-02 02:03:40 -0300

    Update routes name

--- src/routes.tsx
+++ src/routes.tsx
@@ -8,7 +8,7 @@ const { Navigator, Screen } = createBottomTabNavigator();
 const Routes: React.FC = () => (
   <NavigationContainer>
     <Navigator>
-      <Screen name="Following" component={View} />
+      <Screen name="Teste" component={View} />
       <Screen name="Discover" component={View} />
       <Screen name="Browse" component={View} />
       <Screen name="Esports" component={View} />
